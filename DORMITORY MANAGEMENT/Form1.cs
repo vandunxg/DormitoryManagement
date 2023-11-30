59b962a23aea1ea2986bed884d7597c389a414fd
vandunxg <nguyenvandung.official@gmail.com>
2023-11-30 14:56:57 +0700

    update

--- DORMITORY MANAGEMENT/Form1.cs
+++ DORMITORY MANAGEMENT/Form1.cs
@@ -1,19 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
 namespace DORMITORY_MANAGEMENT
 {
     public partial class loginPage : Form
     {
-        
+
         public const int WM_NCLBUTTONDOWN = 0xA1;
         public const int HT_CAPTION = 0x2;
 
@@ -30,7 +24,7 @@ namespace DORMITORY_MANAGEMENT
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            
+
         }
 
         private void bunifuTextBox1_TextChanged(object sender, EventArgs e)
@@ -68,10 +62,64 @@ namespace DORMITORY_MANAGEMENT
 
         private void bunifuLabel3_Click(object sender, EventArgs e)
         {
-            
+
             forgotPasswordPage newForm = new forgotPasswordPage();
             newForm.Show();
             this.Hide();
+        }
+
+        private static bool IsValidEmail(string email)
+        {
+            string regex = @"^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$";
+
+            return Regex.IsMatch(email, regex, RegexOptions.IgnoreCase);
+        }
+
+        private void btn_login_Click(object sender, EventArgs e)
+        {
+            string inputEmailLogin = txt_InputEmailLogin.Text.ToString();
+            string inputPasswordEmail = txt_inputPasswordLogin.Text.ToString();
+
+            if (string.IsNullOrEmpty(inputEmailLogin) || string.IsNullOrEmpty(inputPasswordEmail))
+            {
+                MessageBox.Show("Email và Password không được để trống!", "THÔNG BÁO", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                if (IsValidEmail(inputEmailLogin) == false)
+                {
+                    MessageBox.Show("Email không đúng định dạng!", "THÔNG BÁO", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+                if (inputEmailLogin == "vandunxg@gmail.com" && inputPasswordEmail == "vandunxg")
+                {
+                    Admin_DashBoard adminFormPage = new Admin_DashBoard();
+                    adminFormPage.Show();
+                    this.Hide();
+                }
+                else
+                {
+                    MessageBox.Show("Email hoặc Password không đúng, vui lòng nhật lại!", "THÔNG BÁO", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+
+        }
+
+        private void txt_InputEmailLogin_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == Convert.ToChar(Keys.Enter))
+            {
+                btn_login_Click(sender, e);
+            }
+        }
+
+        private void txt_inputPasswordLogin_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == Convert.ToChar(Keys.Enter))
+            {
+                btn_login_Click(sender, e);
+            }
         }
     }
 }
