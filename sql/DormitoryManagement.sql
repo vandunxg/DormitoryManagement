84c2311c4ae37b5d2a6ba7fc029ef7ca7eda5cfd
Nguyen Van Dung <64525493+vandunxg@users.noreply.github.com>
2023-12-09 21:19:58 -0800

    Update DormitoryManagement.sql

--- sql/DormitoryManagement.sql
+++ sql/DormitoryManagement.sql
@@ -1,8 +1,8 @@
 --Bảng Tài khoản nhân viên
 CREATE TABLE Account(
-  	Account_ID NVARCHAR(10) PRIMARY KEY NOT NULL,
-  	Account_Email NVARCHAR(20) NOT NULL,
-  	Account_Password NVARCHAR(20) NOT NULL
+	Account_ID NVARCHAR(10) PRIMARY KEY NOT NULL,
+	Account_Email NVARCHAR(20) NOT NULL,
+	Account_Password NVARCHAR(20) NOT NULL
 );
 
 -- Bảng Khu
@@ -25,40 +25,49 @@ CREATE TABLE Classroom(
 )
 -- Bảng Phòng
 CREATE TABLE Rooms (
-   	RoomID NVARCHAR(10) PRIMARY KEY NOT NULL,
-    	RoomNumber NVARCHAR(10) NOT NULL,
-    	TypeID NVARCHAR(5) NOT NULL,
+    RoomID NVARCHAR(10) PRIMARY KEY NOT NULL,
+    RoomNumber NVARCHAR(10) NOT NULL,
+    TypeID NVARCHAR(5) NOT NULL,
 	AreaID NVARCHAR(5) NOT NULL,
-    	RoomCapacity INT NOT NULL,
-    	RoomStatus NVARCHAR(20) NOT NULL,
+    RoomCapacity INT NOT NULL,
+    RoomStatus NVARCHAR(20) NOT NULL,
 	FOREIGN KEY (AreaID) REFERENCES Areas(AreaID),
 	FOREIGN KEY (TypeID) REFERENCES TypesRoom(TypeID)
 )
 
+-- Bảng Khoa
+ CREATE TABLE Departments(
+	DepartmentID NVARCHAR(10) PRIMARY KEY NOT NULL,
+	DepartmentName NVARCHAR(30) NOT NULL
+)
+	
+
 -- Bảng Sinh viên
 CREATE TABLE Students (
-    	StudentID NVARCHAR(15) PRIMARY KEY NOT NULL,
-    	StudentName NVARCHAR(20) NOT NULL,
-    	StudentGender NVARCHAR(5) NOT NULL,
-    	StudentDOB DATE NOT NULL,
-    	StudentAddress NVARCHAR(25) NOT NULL,
-    	StudentPhone NVARCHAR(10) NOT NULL,
+    StudentID NVARCHAR(15) PRIMARY KEY NOT NULL,
+    StudentName NVARCHAR(20) NOT NULL,
+    StudentGender NVARCHAR(5) NOT NULL,
+    StudentDOB DATE NOT NULL,
+    StudentAddress NVARCHAR(25) NOT NULL,
+    StudentPhone NVARCHAR(10) NOT NULL,
 	StudentPersonalID NVARCHAR(12) NOT NULL,
 	ClassID NVARCHAR(10) NOT NULL,
+	DepartmentID NVARCHAR(10) NOT NULL,
 	RoomID NVARCHAR(10) NOT NULL,
 	CheckInDate DATE NOT NULL,
-    	CheckOutDate DATE NOT NULL,
-    	StudentLived bit,
+    CheckOutDate DATE NOT NULL,
+    StudentLived bit,
 	StudentRegResident bit,
-    	FOREIGN KEY (RoomID) REFERENCES Rooms(RoomID), 
-	FOREIGN KEY (ClassID) REFERENCES Classroom(ClassID)
+    FOREIGN KEY (RoomID) REFERENCES Rooms(RoomID), 
+	FOREIGN KEY (ClassID) REFERENCES Classroom(ClassID),
+	FOREIGN KEY (DepartmentID) REFERENCES Departments(DepartmentID)
 );
 
 
 -- Bảng Dịch vụ
 CREATE TABLE Services (
-    	ServiceID NVARCHAR(10) PRIMARY KEY NOT NULL,
-    	ServiceElectricity NVARCHAR(10) NOT NULL,
+    ServiceID NVARCHAR(10) PRIMARY KEY NOT NULL,
+    ServiceElectricity NVARCHAR(10) NOT NULL,
 	ServiceWater  NVARCHAR(10) NOT NULL,
 	ServiceTotal  NVARCHAR(10) NOT NULL,
 	ServiceInternet NVARCHAR(10) NOT NULL,
