47dc25760d2dbf4ba8307fc83d8a0bb631214304
Nguyen Van Dung <64525493+vandunxg@users.noreply.github.com>
2024-01-11 10:51:13 +0700

    Update UsageServices.cs

--- DORMITORY MANAGEMENT/DTO/UsageServices.cs
+++ DORMITORY MANAGEMENT/DTO/UsageServices.cs
@@ -1,5 +1,23 @@
 ﻿using System.Data;
 
+/*
+    Author : @vandunxg
+    Github : @vandunxg
+    Blog : vandunxg.vercel.app
+    Leetcode : @vandunxg
+    Facebook : @vandunxg
+*/
+
+/* Copyright (C) 2024 Nguyen Van Dung - All Rights Reserved
+ * You may use, distribute and modify this code under the
+ * terms of the MIT license, which unfortunately won't be
+ * written for another century.
+ *
+ * You should have received a copy of the MIT license with
+ * this file. If not, please write to: vandunxg.dev@gmail.com, or visit : vandunxg.vercel.app
+ */
+
+
 namespace DORMITORY_MANAGEMENT.DTO
 {
     public class UsageServices
