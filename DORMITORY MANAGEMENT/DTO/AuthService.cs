94584718a65ddbeeeed6e5918132fffc294c5642
Nguyen Van Dung <64525493+vandunxg@users.noreply.github.com>
2024-01-11 10:50:44 +0700

    Update AuthService.cs

--- DORMITORY MANAGEMENT/DTO/AuthService.cs
+++ DORMITORY MANAGEMENT/DTO/AuthService.cs
@@ -1,5 +1,24 @@
 ﻿namespace DORMITORY_MANAGEMENT.DTO
 {
+
+/*
+    Author : @vandunxg
+    Github : @vandunxg
+    Blog : vandunxg.vercel.app
+    Leetcode : @vandunxg
+    Facebook : @vandunxg
+*/
+
+/* Copyright (C) 2024 Nguyen Van Dung - All Rights Reserved
+ * You may use, distribute and modify this code under the
+ * terms of the MIT license, which unfortunately won't be
+ * written for another century.
+ *
+ * You should have received a copy of the MIT license with
+ * this file. If not, please write to: vandunxg.dev@gmail.com, or visit : vandunxg.vercel.app
+ */
+
+
     public class AuthService
     {
         private static int loggedInUserId;
