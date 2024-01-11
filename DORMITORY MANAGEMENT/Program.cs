624eb322216daa9335bc88b0c9e2017f848e156e
Nguyen Van Dung <64525493+vandunxg@users.noreply.github.com>
2024-01-11 10:45:49 +0700

    Update Program.cs

--- DORMITORY MANAGEMENT/Program.cs
+++ DORMITORY MANAGEMENT/Program.cs
@@ -4,6 +4,23 @@ using System.Windows.Forms;
 
 namespace DORMITORY_MANAGEMENT
 {
+    /*
+    Author : @vandunxg
+    Github : @vandunxg
+    Blog : vandunxg.vercel.app
+    Leetcode : @vandunxg
+    Facebook : @vandunxg
+*/
+
+/* Copyright (C) 2024 Nguyen Van Dung - All Rights Reserved
+ * You may use, distribute and modify this code under the
+ * terms of the MIT license, which unfortunately won't be
+ * written for another century.
+ *
+ * You should have received a copy of the MIT license with
+ * this file. If not, please write to: vandunxg.dev@gmail.com, or visit : vandunxg.vercel.app
+ */
+    
     internal static class Program
     {
         /// <summary>
